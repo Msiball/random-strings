d7a1aca946b4a047586facce9be822e1b57aa208
Manpreet Singh <manpreet0361.ms@gmail.com>
2024-06-07 18:09:09 -0600

    Add files via upload

---
+++ Random strings.js
@@ -0,0 +1,9 @@
+function whosPaying (names) {
+    var nameList = ["angela","Ben","chloe","dexter","elsa","fred"];
+      
+    var names=nameList[Math.floor(Math.random()*nameList.length)];
+    
+    
+    console.log(names + " is buying todays dinner.")
+           
+    }
